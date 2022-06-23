9587f4ead4c8143acf8bb469085649e104adafa3
Your Name <you@example.com>
2022-06-22 18:22:38 -0700

    newest commit

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,3 +1,4 @@
+from urllib import response
 from flask import Blueprint, request, jsonify, render_template
 from helpers import token_required
 from models import db, User, Contact, contact_schema, contacts_schema
@@ -19,7 +20,7 @@ def create_contact(current_user_token):
     car_color = request.json['car_color']
     user_token = current_user_token.token
 
-    print(f'BIG TESTER: {current_user_token.token}')
+    print(f'BIG TESTER: {user_token}')
 
     contact = Contact(name, email, car_year, car_make, car_model, car_color, user_token = user_token)
 
@@ -31,13 +32,13 @@ def create_contact(current_user_token):
 
 @api.route('/contacts', methods = ['GET'])
 @token_required
-def get_contact(current_user_token):
+def getContact(current_user_token):
     a_user = current_user_token.token
     contacts = Contact.query.filter_by(user_token = a_user).all()
     response = contacts_schema.dump(contacts)
     return jsonify(response)
 
-@api.route('/contacts/<id>', methods = ['GET'])
+@api.route('/contacts/<id>', methods=['GET'])
 @token_required
 def get_single_contact(current_user_token, id):
     contact = Contact.query.get(id)
